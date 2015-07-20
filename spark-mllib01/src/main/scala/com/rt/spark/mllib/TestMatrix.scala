3395c1f7ff9910c24ece82c9b6f41ead1115b6bd
thomaztony <thomaztony@gmail.com>
2015-07-20 20:14:42 +0530

    commit

--- spark-mllib01/src/main/scala/com/rt/spark/mllib/TestMatrix.scala
+++ spark-mllib01/src/main/scala/com/rt/spark/mllib/TestMatrix.scala
@@ -1,20 +1,34 @@
-package com.example.spark.mllib
+package com.rt.spark.mllib
 
-import org.apache.spark.mllib.linalg.Matrices
-import org.apache.spark.mllib.linalg.Matrix
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.{Matrix, Matrices}
+
+
 
 object TestMatrix {
-	def main(args: Array[String]) : Unit = {
-	  	//creating a dense matrix of size 3x2
+	def main(args: Array[String]): Unit = {
+	  /*
+	   * Local matrix 
+	   * integer typed row and column indices
+	   * double typed values
+	   * stored on a single machine.
+	   * 
+	   * MLlib supports dense matrices whose entry values are stored in a single
+	   * double array in column major format
+	   */
+	    //creating a dense matrix of size 3x2
 		val dm: Matrix = Matrices.dense(3, 2, Array(1.0,3.0,5.0,2.0,4.0,6.0))
 		println("Printing 3x2 matrix :")
-		println (dm) 
-			
-		/*val a = sc.parallelize(List(1,2,3,3,5,6,7))
+		println (dm)	
 		
-		val rows: RDD[Vector] =  
-		val mat: */
-		
+		/*
+		 * Distributed matrix
+		 * long typed row and column indices
+		 * double typed values
+		 * stored in one or more RDD's
+		 * 3 types are implemented
+		 * Row matrix
+		 * Indexed row matrix
+		 * Coordinate matrix
+		 */
 	}
 }
